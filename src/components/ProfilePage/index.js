85196f1b0532696787d6c3cbe522af09954161ef
Ronaldo <kikogamer@gmail.com>
2019-05-28 00:04:17 -0300

    UserInfo component

--- src/components/ProfilePage/index.js
+++ src/components/ProfilePage/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Row, Col } from 'react-materialize';
+import UserInfoContainer from '../../containers/UserInfoContainer'
 import MainLayout from '../MainLayout'
 
 const ProfilePage = () => (
   <MainLayout>
     <Row>
       <Col s={12} m={3}>
-        User Info
+        <UserInfoContainer />
       </Col>
       <Col s={12} m={6}>
         Tweet List
