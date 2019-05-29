b6c415bfc2e707958199c41286e5e8aa300035a7
Ronaldo <kikogamer@gmail.com>
2019-05-28 22:36:13 -0300

    trending topics getting data from api

--- src/containers/TrendingTopicsContainer/index.js
+++ src/containers/TrendingTopicsContainer/index.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import TrendingTopics from '../../components/TrendingTopics'
+  import TrendingTopics from '../../components/TrendingTopics'
+  import axios from 'axios';
 
-class TrendingTopicsContainer extends Component {
-  constructor(props) {
-    super(props);
+  class TrendingTopicsContainer extends Component {
+    constructor(props) {
+      super(props);
 
-    this.state = {
-      hashtags: []
-    };
-  }
+      this.state = {
+        hashtags: [],
+      };
+    }
 
-  componentDidMount() {
-    this.setState({
-      hashtags: [['#OneBitCode', '10k'], ['#RubyOnRails', '10k']]
-    })
-  }
+    componentDidMount() {
+    axios.get(process.env.REACT_APP_API + '/trending')
+      .then((response)=>{
+        console.log(response.data.hashtags); 
+        this.setState(()=>{
+          return {
+            hashtags: response.data.hashtags
+          }
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
 
-  render() {
-    return (
-      <div>
+    render() {
+      return (
+        <div>
         <TrendingTopics hashtags={this.state.hashtags} />
       </div>
-    );
+      );
+    }
   }
-}
 
 export default TrendingTopicsContainer;
