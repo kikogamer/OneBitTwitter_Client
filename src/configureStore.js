9665ca4d3903c46f8503deb9f531ce4988156062
Ronaldo <kikogamer@gmail.com>
2019-06-02 23:13:06 -0300

    configure project to use redux dev tools

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,11 +4,14 @@ import { routerMiddleware } from 'connected-react-router'
 import createRootReducer from './reducers'
 import thunk from 'redux-thunk';
 
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 export const history = createBrowserHistory()
 
 export default function configureStore(preloadedState) {
   const store = createStore(
     createRootReducer(history), // root reducer with router state
+    devtools,
     preloadedState,
     compose(
       applyMiddleware(
