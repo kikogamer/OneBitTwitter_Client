7e4b6b906aae247b1d1c8d999ba06a6c17152d55
Ronaldo <kikogamer@gmail.com>
2019-06-13 00:27:44 -0300

    timeline page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import HomePageContainer from './containers/HomePageContainer'
 import ProfilePageContainer from './containers/ProfilePageContainer'
+import TimelineContainer from './containers/TimelineContainer'
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path="/" component={HomePageContainer} /> 
+          <Route exact path="/" component={HomePageContainer} />
           <Route exact path="/user/:id" component={ProfilePageContainer} />
+          <Route exact path="/timeline" component={TimelineContainer} />
         </Switch>
       </div>
     );
---
+++ src/components/Timeline/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Row } from 'react-materialize';
+import MainLayout from '../MainLayout'
+
+
+const Timeline = () => (
+  <MainLayout>
+    <Row>
+      <p>Timeline here</p>
+    </Row>
+  </MainLayout>
+);
+
+export default Timeline;
